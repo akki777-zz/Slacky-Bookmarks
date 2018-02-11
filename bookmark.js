e5cd1e59e7ab591c24807f8407ef26dacf6b6644
akki777 <aksh.mahajan007@gmail.com>
2018-02-11 13:23:29 +0530

    Added config.js

--- bookmark.js
+++ bookmark.js
@@ -1,9 +1,3 @@
-const token = "token=<YOUR_CUSTOM_SLACK_BOT_TOKEN>";    //starting with `xoxb`
-const channel = "&channel=@<YOUR_SLACK_USERNAME>"; // e.g. @john_wayne
-const messageOptions = "&unfurl_links=true&pretty=1&as_user=true";
-const slackUrl = "https://slack.com/api/chat.postMessage?" + token + channel + messageOptions;  // Magic Url
-
-
 /* Create Context Menu and Handle click */
 chrome.contextMenus.create({type: "normal", id: "101", title: "Slack-this", onclick: function(info) {
     sendToSlack(info.pageUrl);
